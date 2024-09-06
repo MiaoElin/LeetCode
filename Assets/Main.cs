379bce84bb78f3f1d653bded0b46d11b5f70b5eb
MiaoElin <249176332@qq.com>
2024-09-06 21:41:31 +0800

    <feature> add: 合并两个有序链表

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -988,6 +988,55 @@ public class Main : MonoBehaviour {
     }
     #endregion
 
+    #region 合并两个有序链表
+    public static ListNode MergeTwoLists_1(ListNode list1, ListNode list2) {
+
+        if (list1 == null && list2 == null) {
+            return list1;
+        }
+
+        List<ListNode> all = new List<ListNode>();
+
+        while (list1 != null || list2 != null) {
+            if (list1 == null) {
+                all.Add(list2);
+                list2 = list2.next;
+                continue;
+            }
+            if (list2 == null) {
+                all.Add(list1);
+                list1 = list1.next;
+                continue;
+            }
+
+            if (list1.val <= list2.val) {
+                all.Add(list1);
+                list1 = list1.next;
+            } else {
+                all.Add(list2);
+                list2 = list2.next;
+            }
+        }
+        for (int i = 0; i < all.Count - 1; i++) {
+            all[i].next = all[i + 1];
+        }
+        return all[0];
+    }
+
+    public static ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        } else if (list1.val < list2.val) {
+            list1.next = MergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = MergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+    #endregion
 
 
 
